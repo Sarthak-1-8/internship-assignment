6aeceafcf3f38aa8ae6bc2e65b4736423fd35f21
Sarthak Patel <patelsarthak759@gmail.com>
2025-06-05 14:48:51 +0530

    temp comment for triggring pipeline

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = 3000;
 app.get('/', (req, res) => {
     res.send('🚀 Node.js app deployed with GitHub Actions!');
 });
-
+// temp comment for triggering pipeline
 app.listen(PORT, () => {
     console.log(`App running on http://localhost:${PORT}`);
 });
